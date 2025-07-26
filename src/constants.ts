2fd6561a85aeec4d2a95594a16574d39f000b622
litchdoteth <0xteksage@gmail.com>
2025-07-26 19:04:21 +0800

    Refactor constants.ts for code style consistency
    Updated formatting to use consistent semicolons and double quotes, reordered USDC and BONK pools, and improved readability with clearer function and array formatting. No functional changes were made.

--- src/constants.ts
+++ src/constants.ts
@@ -1,37 +1,46 @@
-import { PublicKey } from '@solana/web3.js'
-import { FAKE_TOKEN_MINT, PoolToken, TokenMeta, makeHeliusTokenFetcher } from 'gamba-react-ui-v2'
+import { PublicKey } from "@solana/web3.js";
+import {
+  FAKE_TOKEN_MINT,
+  PoolToken,
+  TokenMeta,
+  makeHeliusTokenFetcher,
+} from "gamba-react-ui-v2";
 
 // Get RPC from the .env file or default to the public RPC.
-export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT
+export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT;
 
 // Solana address that will receive fees when somebody plays on this platform
 export const PLATFORM_CREATOR_ADDRESS = new PublicKey(
-  'cobw9YgxENUhGCLZ3TsEpXCtyJCBKNWFV72b8K973zx',
-)
+  "cobw9YgxENUhGCLZ3TsEpXCtyJCBKNWFV72b8K973zx"
+);
 
 // Gamba explorer URL - Appears in RecentPlays
-export const EXPLORER_URL = 'https://explorer.gamba.so'
+export const EXPLORER_URL = "https://explorer.gamba.so";
 
 // Platform URL - Appears in ShareModal
-export const PLATFORM_SHARABLE_URL = 'play.casinoofbonk.fun'
+export const PLATFORM_SHARABLE_URL = "play.casinoofbonk.fun";
 
 // Creator fee (in %)
-export const PLATFORM_CREATOR_FEE = 0.01 // 1% !!max 5%!!
+export const PLATFORM_CREATOR_FEE = 0.01; // 1% !!max 5%!!
 
 // Jackpot fee (in %)
-export const PLATFORM_JACKPOT_FEE = 0.001 // 0.1%
+export const PLATFORM_JACKPOT_FEE = 0.001; // 0.1%
 
 // Referral fee (in %)
-export const PLATFORM_REFERRAL_FEE = 0.0025 // 0.25%
+export const PLATFORM_REFERRAL_FEE = 0.0025; // 0.25%
 
 /** If the user should be able to revoke an invite after they've accepted an invite */
-export const PLATFORM_ALLOW_REFERRER_REMOVAL = true
+export const PLATFORM_ALLOW_REFERRER_REMOVAL = true;
 
 // Just a helper function
-const lp = (tokenMint: PublicKey | string, poolAuthority?: PublicKey | string): PoolToken => ({
+const lp = (
+  tokenMint: PublicKey | string,
+  poolAuthority?: PublicKey | string
+): PoolToken => ({
   token: new PublicKey(tokenMint),
-  authority: poolAuthority !== undefined ? new PublicKey(poolAuthority) : undefined,
-})
+  authority:
+    poolAuthority !== undefined ? new PublicKey(poolAuthority) : undefined,
+});
 
 /**
  * List of pools supported by this platform
@@ -42,49 +51,50 @@ export const POOLS = [
   // Fake token:
   lp(FAKE_TOKEN_MINT),
   // SOL:
-  lp('So11111111111111111111111111111111111111112'),
-  // USDC:
-  lp('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'),
+  lp("So11111111111111111111111111111111111111112"),
   // BONK:
-  lp('DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263'),
-]
+  lp("DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263"),
+  // USDC:
+  lp("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"),
+];
 
 // The default token to be selected
-export const DEFAULT_POOL = POOLS[0]
+export const DEFAULT_POOL = POOLS[0];
 
 /**
  * List of token metadata for the supported tokens
  * Alternatively, we can provide a fetcher method to automatically fetch metdata. See TOKEN_METADATA_FETCHER below.
  */
-export const TOKEN_METADATA: (Partial<TokenMeta> & {mint: PublicKey})[] = [
+export const TOKEN_METADATA: (Partial<TokenMeta> & { mint: PublicKey })[] = [
   {
     mint: FAKE_TOKEN_MINT,
-    name: 'TCOB',
-    symbol: 'TCOB',
-    image: '/fakemoney.png',
+    name: "TCOB",
+    symbol: "TCOB",
+    image: "/fakemoney.png",
     baseWager: 1e9,
     decimals: 9,
     usdPrice: 0,
   },
   {
-    mint: new PublicKey('85VBFQZC9TZkfaptBWjvUw7YbZjy52A6mjtPGjstQAmQ'),
-    name: 'W',
-    symbol: 'Wormhole',
-    image: 'https://wormhole.com/token.png',
+    mint: new PublicKey("85VBFQZC9TZkfaptBWjvUw7YbZjy52A6mjtPGjstQAmQ"),
+    name: "W",
+    symbol: "Wormhole",
+    image: "https://wormhole.com/token.png",
     baseWager: 1e6,
     decimals: 6,
     usdPrice: 0,
   },
   {
-    mint: new PublicKey('DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263'),
-    name: 'BONK',
-    symbol: 'BONK',
-    image: 'https://quei6zhlcfsxdfyes577gy7bkxmuz7qqakyt72xlbkyh7fysmoza.arweave.net/hQiPZOsRZXGXBJd_82PhVdlM_hACsT_q6wqwf5cSY7I',
+    mint: new PublicKey("DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263"),
+    name: "BONK",
+    symbol: "BONK",
+    image:
+      "https://quei6zhlcfsxdfyes577gy7bkxmuz7qqakyt72xlbkyh7fysmoza.arweave.net/hQiPZOsRZXGXBJd_82PhVdlM_hACsT_q6wqwf5cSY7I",
     baseWager: 1e6,
     decimals: 6,
     usdPrice: 0,
   },
-]
+];
 
 /** HTML to display to user that they need to accept in order to continue */
 export const TOS_HTML = `
@@ -97,22 +107,19 @@ export const TOS_HTML = `
   <p><b>7. Fair Play:</b> Games are conducted fairly and transparently.</p>
   <p><b>8. Data Privacy:</b> Your privacy is important to us.</p>
   <p><b>9. Responsible Gaming:</b> Play responsibly; seek help if needed.</p>
-`
+`;
 
 /**
  * A method for automatically fetching Token Metadata.
  * Here we create a fetcher that uses Helius metadata API, if an API key exists as an environment variable.
  */
-export const TOKEN_METADATA_FETCHER = (
-  () => {
-    if (import.meta.env.VITE_HELIUS_API_KEY) {
-      return makeHeliusTokenFetcher(
-        import.meta.env.VITE_HELIUS_API_KEY,
-        { dollarBaseWager: 1 },
-      )
-    }
+export const TOKEN_METADATA_FETCHER = (() => {
+  if (import.meta.env.VITE_HELIUS_API_KEY) {
+    return makeHeliusTokenFetcher(import.meta.env.VITE_HELIUS_API_KEY, {
+      dollarBaseWager: 1,
+    });
   }
-)()
+})();
 
-export const ENABLE_LEADERBOARD = true 
-export const ENABLE_TROLLBOX = true // Requires setup in vercel (check tutorial in discord)
+export const ENABLE_LEADERBOARD = true;
+export const ENABLE_TROLLBOX = true; // Requires setup in vercel (check tutorial in discord)
